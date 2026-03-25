4443a9ff9e988f1711505f901595ca31c2a2ce2d
AntonSteegmans <anton.steegmans@gmail.com>
2026-03-25 11:38:28 +0100

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import RauCommandCenter from './RauCommandCenter'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RauCommandCenter />
+  </React.StrictMode>
+)
